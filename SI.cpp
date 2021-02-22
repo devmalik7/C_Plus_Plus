48b44ef2f121504bded4315844eda97fe84050c6
devmalik7 <devm333666@gmail.com>
2021-02-22 23:12:20 +0530

    Create SI.cpp

---
+++ SI.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cout<<"enter the Principal,Rate,Time"<<endl;
+    cin>>x;
+    cin>>y;
+    cin>>z;
+    
+    cout<<"The Si is ="<<(x*y*z)/100;
+    return 0;
+}
