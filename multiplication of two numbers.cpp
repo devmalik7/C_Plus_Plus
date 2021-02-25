e8a9b03a0aef72744b7db29b7488c863bb11e233
devmalik7 <devm333666@gmail.com>
2021-02-26 00:09:01 +0530

    Create multiplication of two numbers.cpp

---
+++ multiplication of two numbers.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+  cout<<"enter the 1st number"<<endl;
+  cin>>a;
+
+
+    cout<<"Enter 2ed the number"<<endl;
+    cin>>b;
+    cout<<"The product is "<<a*b;
+    return 0;
+
+
+}
